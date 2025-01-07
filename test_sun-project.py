69572514498c1a7e78d551aa856ba84a04743271
Sunithadodda <33749912+Sunithadodda@users.noreply.github.com>
2025-01-07 19:37:46 +0530

    Update test_sun-project.py

--- test_sun-project.py
+++ test_sun-project.py
@@ -1,8 +1,8 @@
 # test_sun_project.py
 
-def test_addition(a, b):
+def test_addition( a, b):
     return a + b
 
 def test_addition_valid():
-    assert test_addition(1, 3) == 4  # Passes
-    assert test_addition(1, 4) == 5  # passes
+    assert test_addition( 1, 3) == 4  # Passes
+    assert test_addition( 1, 4) == 5  # passes
